d02f00477895d8c8a118618dc0ea6502c4195e4a
amituidev <amitmishra009v@gmail.com>
2018-08-02 12:31:06 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "./app/index.html"),
-  filename: "./index.html"
+  template: path.join(__dirname, './app/index.html'),
+  filename: './index.html'
 });
 
 module.exports = {
-  entry: { main: "./app/index.js" },
+  entry: { main: './app/index.js' },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[chunkhash].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }, { loader: "eslint-loader" }]
+        use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }]
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               sourceMap: true
             }
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sourceMap: true
             }
@@ -43,14 +43,14 @@ module.exports = {
     ]
   },
 
-  plugins: [new CleanWebpackPlugin("dist", {}), htmlWebpackPlugin],
+  plugins: [new CleanWebpackPlugin('dist', {}), htmlWebpackPlugin],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
   stats: {
     colors: true
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     port: 3000
   }
