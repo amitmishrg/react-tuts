ff0e714662152265827ff0fb28d2e7f15572e321
amituidev <amitmishra009v@gmail.com>
2018-08-14 09:04:43 +0530

    lifecycle

--- app/container/index.js
+++ app/container/index.js
@@ -3,34 +3,71 @@ import ErrorBoundary from "../components/error-boundary";
 import Info from "../components/info";
 
 class App extends Component {
+  // Initialization
   constructor(props) {
-    console.log('constructor');
+    console.log("constructor initialization");
     super(props);
     this.state = {
       count: 0
     };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  // Mounting
+  componentWillMount() {
+    console.log("component Will mount");
   }
 
   componentDidMount() {
     console.log("component Did mount");
-    this.setState({ count: 1 });
+  }
+
+  // Updating
+  componentWillReceiveProps(newProps) {
+    console.log("componentWillReceiveProps", newProps);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldComponentUpdate", this.state, nextState);
+    console.log("shouldComponentUpdate", this.state, nextProps, nextState);
     return true;
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate", nextProps, nextState);
+  }
+
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   // Are we adding new items to the list?
+  //   // Capture the scroll position so we can adjust scroll later.
+  //   console.log("getSnapshotBeforeUpdate", prevProps, prevState);
+  //   return null;
+  // }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("componentDidUpdate", prevState);
+    console.log("componentDidUpdate", prevProps, prevState);
+  }
+
+  changeState() {
+    this.setState(prevState => {
+      return { count: prevState.count + 1 };
+    });
   }
 
   render() {
-    console.log('render');
+    console.log("render");
     return (
-        <h1>Count {this.state.count}</h1>
+      <React.Fragment>
+        <button onClick={this.changeState}>Set</button>
+        <h1>
+          Count {this.state.count} {this.props.title}
+        </h1>
+      </React.Fragment>
     );
   }
 }
+
+App.defaultProps = {
+  title: "value"
+};
 
 export default App;
